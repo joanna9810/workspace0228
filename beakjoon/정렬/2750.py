0737d9916080945cc97aab6b529a44dd6e1bbcf5
joanna9810 <joannajang98@gmail.com>
2022-05-10 11:44:59 +0900

    git config --local commit.template <path_to_template_file>

--- beakjoon/정렬/2750.py
+++ beakjoon/정렬/2750.py
@@ -33,3 +33,81 @@ arr.sort() # 정렬
 
 for i in range(n): # 정렬된 대로 하나하나 뽑아내기
     print(arr[i])
+
+print('-'*50)
+
+# merge sort
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid]) # sort the left half of the array (assuming n > 1)
+    right = merge_sort(array[mid:]) # sort the right half of the array (assuming n > 1)
+    i,j,k = 0,0,0
+
+    while i < len(left) and j < len(right): # i, j 가 가리키는 값 비교-> 작은 값을 k에 넣기
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+
+        if i == len(left): # 한쪽 리스트가 끝난 경우, 나머지 리스트를 넣어줌
+            while j < len(right):
+                array[k] = right[j]
+                j += 1
+                k += 1
+        elif j == len(right):
+            while j < len(left):
+                array[k] = left[i]
+                i += 1
+                k += 1
+        return array
+
+n = int(input())
+array = []
+
+for _ in range(n):
+    array.append(int(input()))
+
+array = merge_sort(array) # merge the two halves together
+
+for data in array:
+    print(data)
+
+
+print('-'*50)
+
+# Bubble sort
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+for i in range(len(arr)):
+    for j in range(len(arr)):
+      if arr[i] < arr[j]:
+          arr[i], arr[j] = arr[j], arr[i]
+
+for m in arr:
+    print(m)
+
+print('-'*50)
+
+# sys.stdin.readline()
+# => input() 과 비교해서 prompt message을 인수로 받지 않고
+# 개행 문제를 삭제하지 않기 때문에 더 빠름
+
+import sys
+a = sys.stdin.readline().strip()
+arr = []
+
+for _ in range(a):
+    arr.append(int(sys.stdin.readline()))
+
+arr.sort()
+
+for data in arr:
+    print(data)
