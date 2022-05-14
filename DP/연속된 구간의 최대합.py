f50070ce306dc127060f3631968dc2fb4e09a87c
joanna9810 <joannajang98@gmail.com>
2022-05-14 09:50:14 +0900

    git config --local commit.template <path_to_template_file>

--- DP/연속된 구간의 최대합.py
+++ DP/연속된 구간의 최대합.py
@@ -32,10 +32,9 @@ array.append(map(int, input().split()))
 # 앞서 계산된 결과를 저장하기 위한 dp 테이블 생성
 d = [0] * 100001
 # 다이나믹 프로그램 진행 (바텀업)
-d[0] = 0
-d[1] = max(array[0], array[1])
+
 for i in range(2, n):
-    d[i] = max(d[i-1]+d[i-2]+array[1])
+    d[i] = max(d[i-1]+d[i-2])
 # 계산된 결과 출력
 if d[n] == 100001:
     print(-1)
