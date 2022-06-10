059e1ec4bf4787d600947fed35baa4843e44d477
joanna9810 <joannajang98@gmail.com>
2022-06-11 07:56:41 +0900

    git config --local commit.template <path_to_template_file>

--- beakjoon/greedy/문자열 뒤집기.py
+++ beakjoon/greedy/문자열 뒤집기.py
@@ -1,17 +1,15 @@
 # 연속된 숫자를 뒤집을 때 해야 하는 최소 행동 수 구하기
 s = input()
-now = int(s[0])
-cnt = 0
-for i in range(1, len(s)):
-    num = int(s[i])
-    if now != num:
-        new = num
-        cnt += 1
-        if new == num:
+cnt0 = 0
+cnt1 = 0
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        if s[i+1] == '0':
+            cnt0 += 1
+        elif s[i+1] == '1':
+            cnt1 += 1
 
-
-
-print(cnt)
+print(min(cnt0, cnt1))
 
 print('-'*50)
 #
