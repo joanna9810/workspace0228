5c488616af4ce3d8b286f0c4f8dafffe08cb0bc6
joanna9810 <joannajang98@gmail.com>
2022-06-11 22:37:43 +0900

    git config --local commit.template <path_to_template_file>

--- beakjoon/greedy/만들 수 없는 금액.py
+++ beakjoon/greedy/만들 수 없는 금액.py
@@ -1,7 +1,13 @@
 n = int(input())
-coins = list(map(str,input().split()))
-max_coins = sum(int(coins))
-for i in range(1, max_coins):
-    if coins[i] != max_coins[i] or coins[i] != max_coins[i] + max_coins[i]:
-        print(i)
+data = list(map(int, input().split()))
+data.sort()
 
+target = 1
+for x in data:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
